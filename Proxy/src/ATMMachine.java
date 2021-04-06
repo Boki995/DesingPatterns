b7db6d90e44a45b1db262f4e48fa1f20a1ab05af
Boki995 <bojan.milijanovic@gmail.com>
2021-04-06 17:06:28 +0200

    Add files via upload

---
+++ Proxy/src/ATMMachine.java
@@ -0,0 +1,18 @@
+
+public class ATMMachine implements GetATMData {
+	
+	public ATMState getYesCardState(){return hasCard;}
+
+	@Override
+	public ATMState getATMState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getCashInMachine() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Proxy/src/ATMState.java
@@ -0,0 +1,9 @@
+
+public interface ATMState {
+	
+	void insertCard();
+	void ejectCard();
+	void insertPin(int pinEntered);
+	void requestCash(int cashToWithdraw);
+
+}
