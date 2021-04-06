8d8d9b8cbda751a9019274dd3e35e3d789d65e9c
Boki995 <bojan.milijanovic@gmail.com>
2021-04-06 17:05:12 +0200

    Add files via upload

---
+++ Memento/src/Memento.java
@@ -0,0 +1,17 @@
+
+public class Memento {
+	
+	private String article;
+
+	public Memento(String articleSave) {
+		
+		article=articleSave;
+		
+		
+	
+	}
+	
+	
+	public String getArticleSaved(){return article;}
+
+}
